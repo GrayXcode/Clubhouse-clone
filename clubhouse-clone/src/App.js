4778cc8d2d2d88623c1ac5e9d7ddcf8bfa87500b
GrayXcode <hoyx0101@gmail.com>
2022-05-30 21:07:23 +0100

    ADD: add app layout

--- clubhouse-clone/src/App.js
+++ clubhouse-clone/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
 import CodeConfirm from "./pages/CodeConfirm";
 import AllowNotification from "./pages/AllowNotification";
+import AppLayout from "./pages/Layouts/AppLayout";
 
 function App() {
   return (
@@ -13,10 +14,18 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Welcome />} />
           <Route path="/invite" element={<PhoneConfirmation />} />
-          <Route path="/code_confirm" element={<CodeConfirm />} />
+          <Route path="/code_confirm" s element={<CodeConfirm />} />
           <Route path="/allow_notification" element={<AllowNotification />} />
         </Routes>
       </PlanLayout>
+
+      <Route exact path={["/home"]}>
+        <AppLayout>
+          <Routes>
+            <Route exact path="/home" element={<Home />} />
+          </Routes>
+        </AppLayout>
+      </Route>
     </BrowserRouter>
   );
 }
