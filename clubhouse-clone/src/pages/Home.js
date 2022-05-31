781c19616f9f85068f05f0b3b008a49f160b936b
GrayXcode <hoyx0101@gmail.com>
2022-05-31 10:23:39 +0100

    ADD: add react-icons

--- clubhouse-clone/src/pages/Home.js
+++ clubhouse-clone/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import style from "../style/home.module.css";
+import DailyInfoCard from "../components/DailyInfoCard";
 import Header from "../components/Header";
+import RoomInfoCard from "../components/RoomInfoCard";
 
 function Home() {
   return (
     <>
       <Header />
+      <div className={style.home_container}>
+              <DailyInfoCard />
+              <RoomInfoCard />
+      </div>
     </>
   );
 }
