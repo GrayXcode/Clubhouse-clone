1e9db29a74ab521cd6ed4fc0b4b887a3fcadb1cd
GrayXcode <hoyx0101@gmail.com>
2022-05-31 07:05:44 +0100

    ADD: add data folder which contain my json data

--- clubhouse-clone/src/App.js
+++ clubhouse-clone/src/App.js
@@ -1,29 +1,31 @@
 import "./App.css";
 import PlanLayout from "./pages/Layouts/PlanLayout";
 import Welcome from "./pages/Welcome";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
 import CodeConfirm from "./pages/CodeConfirm";
 import AllowNotification from "./pages/AllowNotification";
 import AppLayout from "./pages/Layouts/AppLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <PlanLayout>
-        <Routes>
-          <Route exact path="/" element={<Welcome />} />
-          <Route path="/invite" element={<PhoneConfirmation />} />
-          <Route path="/code_confirm" s element={<CodeConfirm />} />
-          <Route path="/allow_notification" element={<AllowNotification />} />
-        </Routes>
-      </PlanLayout>
-
-      <Route exact path={["/home"]}>
+      <Route path={["/", "/invite", "/code_confirm", "/allow_notification"]}>
+        <PlanLayout>
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route path="/invite" component={PhoneConfirmation} />
+            <Route path="/code_confirm" component={CodeConfirm} />
+            <Route path="/allow_notification" component={AllowNotification} />
+          </Switch>
+        </PlanLayout>
+      </Route>
+      <Route path={["/home"]}>
         <AppLayout>
-          <Routes>
-            <Route exact path="/home" element={<Home />} />
-          </Routes>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+          </Switch>
         </AppLayout>
       </Route>
     </BrowserRouter>
