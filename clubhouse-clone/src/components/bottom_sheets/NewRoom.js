d5dacd03b6a043291d9464c7eb36a0dd4c54dbca
GrayXcode <hoyx0101@gmail.com>
2022-05-31 22:13:15 +0100

    ADD: build home footer

--- clubhouse-clone/src/components/bottom_sheets/NewRoom.js
+++ clubhouse-clone/src/components/bottom_sheets/NewRoom.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import style from "../../style/roomDetail.module.css";
-import { AiOutlineFile } from "react-icons/ai";
-import { BsMicMuteFill } from "react-icons/bs";
+import { AiOutlineFile, AiOutlinePlus } from "react-icons/ai";
+import { BsMicMuteFill, BsMicFill, BsMicMute } from "react-icons/bs";
 
 function NewRoom(props) {
   const [micMuteVisible, setMicMuteVisible] = useState(false);
+  const [itemsVisible, setItemsVisible] = useState(true);
 
   const card = props.cardDetail;
   return (
@@ -33,6 +34,50 @@ function NewRoom(props) {
               alt=""
               className={style.profile_img}
             />
+          </div>
+        </div>
+        {/*End of header */}
+        <div className={style.roomDetailCard}>
+          <div
+            className="d-flex align-items-center justify-content-between flex-wrap"
+            style={{ padding: "0.5em 1em" }}
+          >
+            {card.members.map((item) => (
+              <div className={style.memberContainer}>
+                {micMuteVisible ? (
+                  <div className={style.audio_icon}>
+                    <BsMicMuteFill />
+                  </div>
+                ) : (
+                  ""
+                )}
+                <img src="/images/user-img.jpg" alt="" />
+                <p>
+                  <span>*</span>
+                  {item.first_name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={style.footer}>
+          <button
+            onClick={() => {
+              props.setSheetVisible(false);
+            }}
+          >
+            <img src="/images/hand-peace.png" alt="" />
+          </button>
+          <div>
+            <button>
+              <AiOutlinePlus />
+            </button>
+            <button>
+              <img src="/images/stopHandIcon.png" alt="" />
+            </button>
+            <button onClick={() => setMicMuteVisible(!micMuteVisible)}>
+              {micMuteVisible ? <BsMicMuteFill /> : <BsMicFill />}
+            </button>
           </div>
         </div>
       </div>
